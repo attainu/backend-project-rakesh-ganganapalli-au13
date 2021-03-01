bf21708bd402cdb5886356503e393641672d8141
rakesh-ganganapalli-au13 <rakeshganganapalli72>
2021-03-01 18:00:42 +0530

    completed creating the admin pages header

--- E-Cart/src/app.js
+++ E-Cart/src/app.js
@@ -4,17 +4,29 @@ require("dotenv").config({path : path.join(__dirname,'.env')});
 const app = express();
 
 
-const InitMongo        =  require("./config/db");
+const DBconnection        =  require("./config/db");
 
-InitMongo();
+//database connection
+DBconnection();
+
+
+//view engine setup
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
 
 
 
+
+
+//basic route
+app.get('/',(req,res)=>{
+    res.render('layouts/adminHeader');
+});
 
 
 
 
 
 app.listen(process.env.PORT,()=>{
-    console.log(`server starting at ${process.env.PORT}`)
+    console.log(`server starting at ${process.env.PORT}`);
 });
